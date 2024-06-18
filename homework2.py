2cc86abd1d906e4a14b1580bb4719e87e7375176
andreyalf1961 <bazulin.a.i@gmail.com>
2024-06-18 20:49:24 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+#Задание по теме "Переменные"
+task_count=12
+task_time=1.5
+course_name='Python'
+time_for_task=task_time/task_count
+print(f'Курс: {course_name}, всего задач: {task_count}, затрачено часов: {task_time}, среднее времявыполнения {time_for_task}')
