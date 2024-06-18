5df8768e9749b9276da7a1d44c28a11df57b06a2
andreyalf1961 <bazulin.a.i@gmail.com>
2024-06-18 21:00:25 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+#Практическая работа по уроку "Динамическая типизация"
+name='Andrey'
+print("name: ",name)
+age=63
+print("age: ",age)
+age-=1
+print("new age: ",age)
+is_student=True
+print("is_student:",is_student)
