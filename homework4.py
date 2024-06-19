ef9f6b4e1c45e81bfe1c9d9f1c632b03e4a266ad
andreyalf1961 <bazulin.a.i@gmail.com>
2024-06-19 19:16:58 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+# Практическая работа по уроку "Организация программ и методы строк"
+
+my_string=input('Привет, введите что-н. с пробелом ')
+print("всего символов: ", len(my_string))
+
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
